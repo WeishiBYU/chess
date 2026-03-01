d79e84ca90952f9beed7d7003aadf50ffb3fddc8
Winston <weishiw@byu.edu>
2026-02-28 20:48:47 -0700

    Working on Service

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,5 +1,8 @@
 package service;
 
+import java.util.UUID;
+
+import dataaccess.DataAccessException;
 import service.requests.LoginRequest;
 import service.requests.LogoutRequest;
 import service.requests.RegisterRequest;
@@ -8,15 +11,20 @@ import service.results.RegisterResult;
 
 public class UserService {
     public RegisterResult register(RegisterRequest registerRequest) {
-        
+
         
     }
 
-	public LoginResult login(LoginRequest loginRequest) {}
+	public LoginResult login(LoginRequest loginRequest) {
+        getUser(loginRequest.username());
 
-	public void logout(LogoutRequest logoutRequest) {}
+    }
 
+	public void logout(LogoutRequest logoutRequest) throws DataAccessException{}
 
+    private String createToken() {
+        return UUID.randomUUID().toString();
+    }
 
 
 }
