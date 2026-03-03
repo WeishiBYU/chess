368d98dbdeb7db91ffa05ac8fefe02e0f428b0ba
Winston <weishiw@byu.edu>
2026-03-02 21:54:09 -0700

    A

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -5,10 +5,10 @@ import chess.ChessPiece;
 
 public class ServerMain {
     public static void main(String[] args) {
-        // var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        // System.out.println("♕ 240 Chess Server: " + piece);
-        Server server = new Server();
-        int port = server.run(8080);
-        System.out.println("Server listening on " + port);
+        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        System.out.println("♕ 240 Chess Server: " + piece);
+        // Server server = new Server();
+        // int port = server.run(8080);
+        // System.out.println("Server listening on " + port);
     }
 }
